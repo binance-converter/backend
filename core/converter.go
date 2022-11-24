ba2c33135bf5f9ab71fe350a6c26910804dc100a
Emil <emil.islmaov@mircod.com>
2022-11-24 14:48:29 +0300

    add converter code

--- core/converter.go
+++ core/converter.go
@@ -1,1 +1,18 @@
 package core
+
+import "errors"
+
+type ConverterPair struct {
+	Currencies []FullCurrency
+}
+
+type Exchange float32
+
+type ThresholdConvertPair struct {
+	ConverterPair ConverterPair
+	Exchange      Exchange
+}
+
+var (
+	ErrorConverterInvalidConverterPair = errors.New("invalid converter pair")
+)
