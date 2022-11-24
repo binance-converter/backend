38c7baf50e89683b46310ecfbfb7cc8b8fff7075
Emil <emil.islmaov@mircod.com>
2022-11-24 21:23:33 +0300

    add init server

--- cmd/backend-server/main.go
+++ cmd/backend-server/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/binance-converter/backend/internal/transport/grpc"
+	"github.com/binance-converter/backend/internal/transport/grpc/handler"
+	"github.com/sirupsen/logrus"
+)
 
+func main() {
+	logger := logrus.New()
+
+	auth := handler.NewAuthHandler(nil)
+	converter := handler.NewConverterHandler(nil)
+	currencies := handler.NewCurrenciesHandler(nil)
+	exchangePlot := handler.NewExchangePlotHandler(nil)
+
+	grpcServer := grpc.NewServer(logger, auth, converter, currencies, exchangePlot)
+
+	grpcServer.ListenAndServe(9000)
 }
