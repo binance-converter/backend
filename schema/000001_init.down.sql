d19bec53a6fa97544f4cae2a9716375cd2e83406
Emil <emil.islmaov@mircod.com>
2022-12-02 16:41:50 +0100

    add postgres migrations

---
+++ schema/000001_init.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE user_converter_pair_thresholds;
+DROP TABLE user_converter_pairs;
+DROP TABLE user_currencies;
+DROP TABLE converter_pairs;
+DROP TABLE currencies;
+DROP TYPE currency_types;
+DROP INDEX user_chat_id;
+DROP TABLE users;
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,52 @@
+CREATE TABLE users
+(
+    id            serial primary key,
+    chat_id       int,
+    user_name     varchar(255),
+    first_name    varchar(255) not null,
+    last_name     varchar(255) not null,
+    language_code varchar(3)
+);
+
+CREATE INDEX user_chat_id ON users (chat_id)
+
+CREATE TYPE currency_types as enum ('classic', 'crypto');
+
+CREATE TABLE currencies
+(
+    id        serial primary key,
+    type      currency_types    not null,
+    code      varchar(3) unique not null,
+    back_code varchar(255)
+);
+
+
+CREATE TABLE converter_pairs
+(
+    level           int                                              not null,
+    first_currency  int references currencies (id) on delete cascade not null,
+    second_currency int references currencies (id) on delete cascade not null,
+    third_currency  int references currencies (id) on delete cascade
+);
+
+CREATE TABLE user_currencies
+(
+    id       serial primary key,
+    user_id  int references users (id) on delete cascade      not null,
+    currency int references currencies (id) on delete cascade not null
+);
+
+CREATE TABLE user_converter_pairs
+(
+    id             serial primary key,
+    user_id        int references users (id) on delete cascade           not null,
+    converter_pair int references converter_pairs (id) on delete cascade not null
+);
+
+CREATE TABLE user_converter_pair_thresholds
+(
+    id             serial primary key,
+    user_id        int references users (id) on delete cascade           not null,
+    converter_pair int references converter_pairs (id) on delete cascade not null,
+    threshold      int                                                   not null
+);
