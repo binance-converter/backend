586d23df05b03c6c07e751dc0de9f41503cf982d
Emil <emil.islmaov@mircod.com>
2022-12-05 10:01:37 +0100

    Add interceptor for validate chat_id

--- internal/transport/grpc/server.go
+++ internal/transport/grpc/server.go
@@ -6,17 +6,29 @@ import (
 	"github.com/binance-converter/backend-api/api/converter"
 	"github.com/binance-converter/backend-api/api/currencies"
 	"github.com/binance-converter/backend-api/api/exchange_plot"
+	"github.com/binance-converter/backend/core"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_logrus "github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"net"
+	"strconv"
 )
 
-type Server struct {
-	Logger *logrus.Logger
+const (
+	chatIdKey = "chat_id"
+)
 
+type AuthService interface {
+	ValidateUserByChatId(ctx context.Context, chatId int) (int, error)
+}
+
+type Server struct {
+	Logger       *logrus.Logger
+	authService  AuthService
 	auth         auth.AuthServer
 	converter    converter.ConverterServer
 	currencies   currencies.CurrenciesServer
@@ -27,7 +39,7 @@ type Server struct {
 
 func NewServer(logger *logrus.Logger, auth auth.AuthServer,
 	converter converter.ConverterServer, currencies currencies.CurrenciesServer,
-	exchangePlot exchange_plot.ExchangePlotServer) *Server {
+	exchangePlot exchange_plot.ExchangePlotServer, authService AuthService) *Server {
 	logrusLogger := logrus.NewEntry(logger)
 	return &Server{
 		Logger:       logger,
@@ -47,7 +59,34 @@ func NewServer(logger *logrus.Logger, auth auth.AuthServer,
 					grpc_recovery.UnaryServerInterceptor(),
 				)),
 		),
+		authService: authService,
 	}
+}
+
+func (s *Server) authInterceptor(
+	ctx context.Context,
+	method string,
+	req interface{},
+	reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption,
+) error {
+	// Logic before invoking the invoker
+	chatIdStr := metadata.ValueFromIncomingContext(ctx, chatIdKey)
+	if chatIdStr != nil && len(chatIdStr) > 0 {
+		chatId, err := strconv.Atoi(chatIdStr[0])
+		if err == nil {
+			userId, err := s.authService.ValidateUserByChatId(ctx, chatId)
+			if err == nil {
+				ctx = core.ContextAddUserId(ctx, userId)
+			}
+		}
+	}
+	// Calls the invoker to execute RPC
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	// Logic after invoking the invoker
+	return err
 }
 
 func (s *Server) ListenAndServe(port int) error {
